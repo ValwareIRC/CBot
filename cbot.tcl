9f150313faf167ccbdb64b99f2693d379119c6e6
Valerie Pond <79415174+ValwareIRC@users.noreply.github.com>
2021-09-04 12:21:09 +0100

    Update cbot.tcl
    make into server lmao

--- cbot.tcl
+++ cbot.tcl
@@ -25,22 +25,27 @@ proc sockReadable {sockChan} {
 	set chan [lindex $lline 2]
 	set text [lindex $lline end]
 	switch -nocase -- [lindex $lline 1] {
-		PING {puts $sockChan "PONG :[lindex $lline 2]"}
-		001 {puts $sockChan "JOIN #test"}
-		PRIVMSG {switch -- [lindex $lline 3] {
-				!quit {
-					puts $sockChan "QUIT :Requested"
-					close $sockChan
-					return
-				}
-			}
-		}
+	# probably won't work lmao
+	#	PING {puts $sockChan "PONG :[lindex $lline 2]"}
+	#	001 {puts $sockChan "JOIN #test"}
+	#	PRIVMSG {switch -- [lindex $lline 3] {
+	#			!quit {
+	#				puts $sockChan "QUIT :Requested"
+	#				close $sockChan
+	#				return
+	#			}
+	#		}
+	#	}
 	}
 }
 set sockChan [socket -async $::server $::port]
 fconfigure $sockChan -translation {auto crlf} -buffering line -blocking 0
-puts $sockChan "NICK $::botnick"
-puts $sockChan "USER $::ident * * :$::realname"
+
+puts $sockChan "PASS $::serverpass"
+puts $sockChan "PROTOCTL EAUTH=$::servername SID=0N0"
+puts $sockChan "PROTOCTL NOQUIT NICKv2 SJOIN SJ3 CLK TKLEXT2 NICKIP ESVID MLOCK EXTSWHOIS SJSBY MTAGS"
+puts $sockChan "SERVER $::servername 1 :$::serverdesc"
+
 fileevent $sockChan readable [list sockReadable $sockChan]
 
 # vwait forever -- don't use vwait in eggdrop
